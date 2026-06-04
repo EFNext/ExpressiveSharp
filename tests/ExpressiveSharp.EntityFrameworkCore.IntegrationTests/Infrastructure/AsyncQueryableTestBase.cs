1858e2be0a47630c4717d73bc72013985c153dd2
Koen <koen@linker.io>
2026-06-04 00:49:52 +0000

    refactor: update SeedStoreData to await async operation and clear change tracker

--- tests/ExpressiveSharp.EntityFrameworkCore.IntegrationTests/Infrastructure/AsyncQueryableTestBase.cs
+++ tests/ExpressiveSharp.EntityFrameworkCore.IntegrationTests/Infrastructure/AsyncQueryableTestBase.cs
@@ -7,7 +7,11 @@ namespace ExpressiveSharp.EntityFrameworkCore.IntegrationTests.Infrastructure;
 public abstract class AsyncQueryableTestBase : EFCoreRelationalTestBase
 {
     [TestInitialize]
-    public Task SeedStoreData() => Context.SeedStoreAsync();
+    public async Task SeedStoreData()
+    {
+        await Context.SeedStoreAsync();
+        Context.ChangeTracker.Clear();
+    }
 
     [TestMethod]
     public async Task AnyAsync_WithPredicate_Executes()
